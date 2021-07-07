25b63878e52b3b2c22d6bdd308141ae99a6a5d2d
P4tCr4ft <patrickcarnivore@gmail.com>
2021-07-07 15:31:11 +0930

    Complete main game logic

--- HeadFirstJavaScript_P4tCr4ft/Chapter8/battleship.js
+++ HeadFirstJavaScript_P4tCr4ft/Chapter8/battleship.js
@@ -1,6 +1,33 @@
-const square1 = x => x * x;
+// Steve Triplett
+// HeadFirst JS Programming Ch 10
+// July 2021
 
-let model = {
+const init = () => {
+  const fireButton = document.getElementById("fireButton");
+  fireButton.onclick = handleFireButton;
+  const guessInput = document.getElementById("guessInput");
+  guessInput.onkeypress = handleKeyPress;
+};
+
+const handleKeyPress = e => {
+  const fireButton = document.getElementById("fireButton");
+  if (e.keyCode === 13) {
+    fireButton.click();
+    return false;
+  }
+}
+
+const handleFireButton = () => {
+  const guessInput = document.getElementById("guessInput");
+  const guess = guessInput.value;
+  controller.processGuess(guess);
+  guessInput.value = "";
+};
+
+// Browser runs init when page is fully loaded
+window.onload = init;
+
+const model = {
   boardSize: 7,
   numShips: 3,
   shipLength: 3,
@@ -20,6 +47,7 @@ let model = {
       // return ship.locations.includes(guess);
        if (ship.locations.includes(guess)) {
         ship.hits[ship.locations.indexOf(guess)] = "hit";
+        console.log(`HIT! with guess ${guess}`);
         view.displayHit(guess);
         view.displayMessage("HIT!");
         console.log(`ship is sunk? ${model.isSunk(ship)}`);
@@ -30,13 +58,13 @@ let model = {
         console.log(`About to return true, guess ${guess}`);
         return true;
       } else {
+        console.log(`MISS with guess ${guess}`);
         view.displayMiss(guess);
         view.displayMessage("You missed...");
         console.log(`About to return false, guess ${guess}`);
         return false;
       }
     });
-
   },
 
   isSunk: ship => {
@@ -46,39 +74,100 @@ let model = {
 
 };
 
-let view = {
+const view = {
   // this method takes a string and displays it (fat arrow syntax)
-  displayMessage: (msg) => {
-    let messageArea = document.getElementById("messageArea");
+  displayMessage: msg => {
+    const messageArea = document.getElementById("messageArea");
     messageArea.innerHTML = msg;
   },
   // this method takes string and displays a hit
-  displayHit: (location) => {
-    let cell = document.getElementById(location);
+  displayHit: location => {
+    const cell = document.getElementById(location);
     cell.setAttribute("class", "hit");
   },
   // this method takes string and displays a miss
-  displayMiss: (location) => {
-    let cell = document.getElementById(location);
+  displayMiss: location => {
+    const cell = document.getElementById(location);
     cell.setAttribute("class", "miss");
   }
 };
 
-let controller = {
+const controller = {
   // this method takes a string and displays it
-  someMethod: function(msg) {
+  guesses: 0,
 
+  processGuess: guess => {
+    const guessLocation = controller.parseGuess(guess);
+    if (guessLocation) {
+      controller.guesses++;
+      const hit = model.fire(guessLocation);
+      if (hit && model.shipsSunk === model.numShips) {
+        view.displayMessage("You sank all my battleships, in " + controller.guesses + " guesses.");
+      }
+    }
+  },
+
+  // We're defining validity as is something, and has length 2
+  parseGuess: guess => {
+    const alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+    let row;
+    let column;
+    if (guess === null || guess.length !== 2) {
+      alert("Please enter a letter and number combo from the board.");
+    } else {
+      row = alphabet.indexOf(guess.charAt(0));
+      column = guess.charAt(1);
+
+      if (isNaN(row) || isNaN(column)) {
+        alert("That guess is not on the board.");
+        // Note use of type conversion below checking column, which is a string
+      } else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize) {
+        alert("That guess is off the board.");
+      } else {
+        return row + column;
+      }
+    }
+    return null;
   }
+
 };
+
+
+// Below are some fire and other method tests
+
+// console.log(`processGuess with A0 is ${controller.processGuess("A0")}`);
+
+// console.log(`processGuess with A6 is ${controller.processGuess("A6")}`);
+// console.log(`processGuess with B6 is ${controller.processGuess("B6")}`);
+// console.log(`processGuess with C6 is ${controller.processGuess("C6")}`);
+
+// console.log(`processGuess with C4 is ${controller.processGuess("C4")}`);
+// console.log(`processGuess with D4 is ${controller.processGuess("D4")}`);
+// console.log(`processGuess with E4 is ${controller.processGuess("E4")}`);
+
+// console.log(`processGuess with B0 is ${controller.processGuess("B0")}`);
+// console.log(`processGuess with B1 is ${controller.processGuess("B1")}`);
+// console.log(`processGuess with B2 is ${controller.processGuess("B2")}`);
+
+
+
+// console.log(`parseGuess with A0 is ${controller.parseGuess("A0")}`);
+// console.log(`parseGuess with B6 is ${controller.parseGuess("B6")}`);
+// console.log(`parseGuess with G3 is ${controller.parseGuess("G3")}`);
+// console.log(`parseGuess with H0 is ${controller.parseGuess("H0")}`);
+// console.log(`parseGuess with A7 is ${controller.parseGuess("A7")}`);
+
 
 // model.fire("34");
 // model.fire("53");
-console.log(`fire method returns ${model.fire("54")}`);
-console.log(`fire method returns ${model.fire("34")}`);
-console.log(`fire method returns ${model.fire("65")}`);
-console.log(`fire method returns ${model.fire("24")}`);
-console.log(`fire method returns ${model.fire("44")}`);
-// console.log(`fire method returns ${model.fire("61")}`);
+
+// console.log(`fire method with guess 54 returns ${model.fire("54")}`);
+// console.log(`fire method with guess 34 returns ${model.fire("34")}`);
+// console.log(`fire method with guess 65 returns ${model.fire("65")}`);
+// console.log(`fire method with guess 24 returns ${model.fire("24")}`);
+// console.log(`fire method with guess 44 returns ${model.fire("44")}`);
+
+// console.log(`fire method with guess 54 returns ${model.fire("61")}`);
 // console.log(model.fire("54"));
 
 
@@ -91,10 +180,14 @@ console.log(`fire method returns ${model.fire("44")}`);
 // view.displayHit("26");
 // view.displayMessage("Anyone home?");
 
+
+
+
+// Below are some test High-Order Functions (ES6)
+
+const square1 = x => x * x;
+
 // console.log(`5 squared is ${square1(5)}`);
-
-
-// view.displayMessage("Hi there");
 
 // console.log([1, 2, 3].every( el => el !== 1));
 // console.log([1, 2, 3].some( el => el !== 1));
